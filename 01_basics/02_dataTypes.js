745a98d74cd9f096d45147cb2fc646f6d84eb6cf
kaisar Hameed <kaisersofi.ks2515@gmail.com>
2024-11-03 05:36:43 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict";       //treat all JS code as newer version
+
+// alert(3+3)  // we are using nodejs not browser
+
+let name= 'kaisar'
+let age = 18
+let isLoggedIn = true
+
+// number => range  2 to power 53
+// bigint
+// String => ""
+// boolean => true/false
+// null => type object standalone value  it is kind of emtpy like in case of temperature 0 is value but as empty or no temp we use null
+// undefined =>  type undefined
+// symbol => when we talk about unique
+
+// object
+
+console.log(typeof undefined);
+console.log(typeof(age));
+
+
