f1ff1e2f58df6f5d45a4d583ef66898c17df1886
kaisar Hameed <kaisersofi.ks2515@gmail.com>
2024-11-03 06:21:13 +0000

    operation done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -7,14 +7,14 @@ let scoreInNumber = Number(score)
 let valueInNumber = Number(value)  
 let valInNumber = Number(undefined)  
 
-console.log(typeof scoreInNumber); // it will give number
-console.log(scoreInNumber); // it will give NaN
+// console.log(typeof scoreInNumber); // it will give number
+// console.log(scoreInNumber); // it will give NaN
 
-console.log(typeof valueInNumber); // it will give number
-console.log(valueInNumber); // it will give 0
+// console.log(typeof valueInNumber); // it will give number
+// console.log(valueInNumber); // it will give 0
 
-console.log(typeof valInNumber); // it will give number
-console.log(valInNumber); // it will give NaN
+// console.log(typeof valInNumber); // it will give number
+// console.log(valInNumber); // it will give NaN
 
 // "33" => 33
 // "33asss" => NaN
@@ -23,17 +23,51 @@ console.log(valInNumber); // it will give NaN
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn); // true
+// console.log(booleanIsLoggedIn); // true
 
 // 1 => true; 0 => false
 // "" => false
 // "kaisar" => true
 
 let someNumber = 33
-
 let StringNumber = String(someNumber)
 
 // StringNumber => String; value => 33
+
+
+
+// *********************** Operations ********************************
+
+let someValue = 3
+let negative = -someValue // ==> -3
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 2);
+// console.log(2 / 2);
+// console.log(2 % 2);
+
+let str = 'kaisar'
+let str2 = ' Hameed'
+let str3 = str + str2
+// console.log(str3); // => kaisar hameed
+
+// console.log("1" + 2); // ==> 12
+// console.log(1 + "2"); // ==> 12
+// console.log("1" + 2 + 2 + 2); // ==> concatenate 1222
+// console.log("1" + 2 + 2 - 2); // ==> concatenate 120
+// console.log("1" + 2 - 1 + 2); // ==> concatenate 13
+// console.log(2 - 1 + "3"); // ==> concatenate 13
+// console.log(2 + 1 + "3"); // ==> concatenate 33
+// console.log("3" + 2 + 1 + "3"); // ==> concatenate 3213
+// console.log("3" + 2 - 1 + "3"); // ==> concatenate 313
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2  //==> all have 2
+
+
 
 
 
